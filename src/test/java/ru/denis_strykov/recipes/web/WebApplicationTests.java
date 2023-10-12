e340186072373a49dd4bad918ab1665af8f8e2e6
Danis Strykov <stryk228@yandex.ru>
2023-10-12 14:11:08 +0300

    init commit

---
+++ src/test/java/ru/denis_strykov/recipes/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denis_strykov.recipes.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
