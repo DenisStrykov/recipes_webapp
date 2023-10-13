a3f03c7d73dcf309c5c63765b70a0daee3d86316
Danis Strykov <stryk228@yandex.ru>
2023-10-13 20:02:32 +0300

    set up SecurityConfig for password by BCryptPasswordEncoder

--- src/main/java/ru/denis_strykov/recipes/web/service/impl/UserServiceImpl.java
+++ src/main/java/ru/denis_strykov/recipes/web/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.denis_strykov.recipes.web.service.impl;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.denis_strykov.recipes.web.dto.RegistrationDto;
 import ru.denis_strykov.recipes.web.models.Role;
@@ -17,11 +18,13 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, RoleRepository repository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository repository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -29,7 +32,7 @@ public class UserServiceImpl implements UserService {
         UserEntity user = new UserEntity();
         user.setUsername(registrationDto.getUsername());
         user.setEmail(registrationDto.getEmail());
-        user.setPassword(registrationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
         Role role = roleRepository.findByName("USER");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
