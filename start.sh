2dc16b307524cc191d8df85ca7e20017d2d65403
Danis Strykov <stryk228@yandex.ru>
2023-10-22 23:40:15 +0300

    make README with start.sh & stop.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ mkdir app4testing
 cd app4testing/ || exit
 git clone https://github.com/DenisStrykov/recipes_webapp
 cd recipes_webapp/ || exit
-chmod u+x stop.sh
 
 sudo docker compose up -d --build
 echo "Please go to link http://localhost:8091/recipes"
